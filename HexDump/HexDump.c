b625c3c5d366e007478a25bc0789a02e3dcc4f22
Tan <duytantran2710@gmail.com>
2025-01-24 21:18:51 -0800

    Implement a function to interprete error messages from GetLastError

--- HexDump/HexDump.c
+++ HexDump/HexDump.c
@@ -1,9 +1,32 @@
 #include <windows.h>
 #include <tchar.h>
 
-void dumpBinary(LPCTSTR lpBinaryFile)
+#define EXECUTE_FAILED -1
+
+void printErrorMessage(DWORD dwErrorCode)
 {
-	
+	// Retrieve the system error message for the last-error code
+
+	LPVOID lpMsgBuf; // Error message
+
+	if (FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		dwErrorCode,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf,
+		0, NULL) == 0)
+	{
+		_tprintf(_T("FormatMessage failed. Error: %d\n"), GetLastError());
+		ExitProcess(dwErrorCode);
+	}
+
+	_tprintf(_T("  > Error %d: %s\n"), dwErrorCode, (LPTSTR)lpMsgBuf);
+
+	LocalFree(lpMsgBuf);
+	ExitProcess(dwErrorCode);
 }
 
 int _tmain(int argc, TCHAR *argv[])
@@ -15,8 +38,8 @@ int _tmain(int argc, TCHAR *argv[])
 	}
 	else
 	{
-		LPCTSTR lpBinaryFile = argv[1];
-		_tprintf(_T("\n Open binary file: %s\n\n"), lpBinaryFile);
+		LPCTSTR lpBinaryFilePath = argv[1];
+		_tprintf(_T("\n Open binary file: %s\n\n"), lpBinaryFilePath);
 	}
 
 	return 0;
